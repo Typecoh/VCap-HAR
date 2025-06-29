b5c9e548fea94fa786044f73eed67242bf16c97d
Typecoh <150497655+Typecoh@users.noreply.github.com>
2025-06-29 13:34:26 +0800

    Add files via upload

---
+++ MyCNN/enhanceNet.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torchvision.datasets import DatasetFolder
+from torch.utils.data import DataLoader
+import torchvision
+class SimpleCNN(nn.Module):
+
+    def __init__(self,ClassNum):
+        super(SimpleCNN, self).__init__()
+        self.ClassNum = ClassNum
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1)
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.conv6 = nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv7 = nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.conv8 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.conv9 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3, stride=1, padding=1)
+        # self.conv7 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(16 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, self.ClassNum)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool1(x) # 224
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x) # 112
+        x = self.conv3(x)
+        x = self.relu(x)
+        x = self.pool(x) # 56
+        x = self.conv4(x)
+        x = self.relu(x)
+        x = self.pool1(x)  # 56
+        x = self.conv5(x)
+        x = self.relu(x)
+        x = self.pool(x)  # 28
+        x = self.conv6(x)
+        x = self.relu(x)
+        x = self.pool1(x)  # 28
+        x = self.conv7(x)
+        x = self.relu(x)
+        x = self.pool(x)  # 14
+        x = self.conv8(x)
+        x = self.relu(x)
+        x = self.pool1(x)  # 14
+        x = self.conv9(x)
+        x = self.relu(x)
+        x = self.pool(x) # 7
+        x = x.view(-1, 16 * 7 * 7)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
